6869dd303618cda906ba357e865669af14de19b4
Bruce Lefebvre <bruce@brucelefebvre.com>
2020-05-15 14:02:57 -0400

    Add random shape generator

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import "./App.css";
 import { TetrisGrid } from "./TetrisGrid";
+import { getRandomShapes } from "./shapes/Generator";
 import { line } from "./shapes/Line";
 import { square } from "./shapes/Square";
 
 export function App() {
   return (
     <div className="App">
-      <TetrisGrid width={10} height={15} shapes={[line(), square(), line(), square(), line(), square()]} />
+      <TetrisGrid width={10} height={15} shapes={getRandomShapes(3)} />
     </div>
   );
 }
---
+++ src/shapes/Generator.ts
@@ -0,0 +1,29 @@
+import { square } from "./Square";
+import { line } from "./Line";
+import { Shape } from "./Shape";
+
+const knownShapes = [() => square(), () => line()];
+
+function getRandomShapeIndex(): number {
+  // Find a number between 0 and the length of knownShapes
+  return Math.floor(Math.random() * knownShapes.length);
+}
+
+const colors = ["blue", "green", "yellow", "red", "pink"];
+
+function getRandomColorIndex(): number {
+  // Find a number between 0 and the length of colors
+  return Math.floor(Math.random() * colors.length);
+}
+
+// Get a number of random shapes (count) each with a random color
+export function getRandomShapes(count: number): Shape[] {
+  const shapes: Shape[] = [];
+  for (let i = 0; i < count; i++) {
+    const randomShape: Shape = knownShapes[getRandomShapeIndex()]();
+    const randomColor: string = colors[getRandomColorIndex()];
+    randomShape.color = randomColor;
+    shapes.push(randomShape);
+  }
+  return shapes;
+}
