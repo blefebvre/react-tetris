f3dd615112b095c6b0a7ffc185be04b14fb896fc
Bruce Lefebvre <bruce@brucelefebvre.com>
2020-05-17 07:21:03 -0400

    Deploy

---
+++ precache-manifest.6fd5691c55d22e512b9521c2446132d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbf1e57c4b8c9a27a1bd0a8cc008e79f",
+    "url": "/react-tetris/index.html"
+  },
+  {
+    "revision": "e20e750ac3abca27ff0a",
+    "url": "/react-tetris/static/css/main.76f7e271.chunk.css"
+  },
+  {
+    "revision": "26a4d2fdf0a145471b3d",
+    "url": "/react-tetris/static/js/2.1cd003d1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tetris/static/js/2.1cd003d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e20e750ac3abca27ff0a",
+    "url": "/react-tetris/static/js/main.0ebe9f36.chunk.js"
+  },
+  {
+    "revision": "231cc7bd47f2228164e4",
+    "url": "/react-tetris/static/js/runtime-main.c19a0945.js"
+  }
+]);
--- src/shapes/Shape.ts
+++
@@ -1,8 +0,0 @@
-export type ShapeMatrix = Array<Array<0 | 1>>;
-
-// Representation of a shape
-export interface Shape {
-  name: string;
-  color?: string;
-  positions: ShapeMatrix[];
-}
