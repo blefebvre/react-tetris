bc5080a4de4c5ec5fcafd324a5d2bf120e9a0d8e
Bruce Lefebvre <bruce@brucelefebvre.com>
2020-05-17 21:22:05 -0400

    Make buttons bigger and disable double tap to zoom.
    Add Footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import { TetrisGrid } from "./TetrisGrid";
 import { getRandomShapes } from "./shapes/Generator";
+import { Footer } from "./Footer";
 import { line } from "./shapes/Line";
 import { square } from "./shapes/Square";
 
@@ -10,6 +11,7 @@ export function App() {
     <div className="App">
       <TetrisGrid width={10} height={15} shapes={getRandomShapes(30)} />
       {/* <TetrisGrid width={10} height={15} shapes={[line(), line(), square(), square(), square(), square(), square()]} /> */}
+      <Footer />
     </div>
   );
 }
---
+++ src/Footer.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export function Footer() {
+  return (
+    <footer>
+      <a href="https://github.com/blefebvre/react-tetris" target="_blank">
+        react-tetris
+      </a>{" "}
+      was made by{" "}
+      <a href="https://twitter.com/brucelefebvre" target="_blank">
+        @brucelefebvre
+      </a>
+    </footer>
+  );
+}
